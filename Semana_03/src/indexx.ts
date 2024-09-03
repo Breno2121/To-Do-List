41d58d43e2b304ea48287bc5aae6f9aeadabf8c4
JOSE BRENO OLIVEIRA DA SILVA <jose_bo_silva@estudante.sc.senai.br>
2024-09-02 21:56:34 -0300

    02-09-24

---
+++ Semana_03/src/indexx.ts
@@ -0,0 +1,22 @@
+import leia from 'readline-sync'
+import ContaBancaria from "./ContaBancaria";
+import Titular from "./Titular";
+import Cadastro from './Cadastro';
+import ContaCorrente from './ContaCorrente';
+
+var NOME = leia.question("Digite o seu nome: ");
+var CPF = leia.question("Digite o seu CPF: ");
+var datanasc = leia.question("Digite a data de nascimento: ");
+
+var cadast = new Cadastro(NOME, CPF, datanasc);
+var t1 = new Titular(NOME, CPF, datanasc); 
+var c1 = new ContaBancaria(t1, "breno@gmail.com");
+
+var cc = new ContaCorrente (t1, "Breno@gmail.com");
+
+c1.consultarsaldo();
+c1.depositar(200);
+c1.sacar(30);
+c1.mostrardados();
+cadast.clientecadastrado();
+
