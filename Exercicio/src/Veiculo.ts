13df17ae0a4be414b0568cbb3b723223da67ec7d
JOSE BRENO OLIVEIRA DA SILVA <jose_bo_silva@estudante.sc.senai.br>
2024-09-09 21:23:25 -0300

    09-09-24

---
+++ Exercicio/src/Veiculo.ts
@@ -0,0 +1,19 @@
+import leia from 'readline-sync'
+
+/*
+
+export default class Veiculo{
+    constructor(){
+
+    }
+
+    public marca: string;
+    public modelo: string;
+    public ano: number;
+    public valor: number;
+    public disponivel: boolean;
+    public historicotransferencia: string;
+    public detalheveiculo: void;
+
+}
+
